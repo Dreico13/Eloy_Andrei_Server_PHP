19719885ff29bdd4fa6a6463aee5eca8fbb45fe6
Dreico13 <dragosandreinicolae@gmail.com>
2022-11-30 19:48:19 +0100

    cambios nuevos

--- ws/_ws.php
+++ ws/_ws.php
@@ -1,7 +1,11 @@
 <?php
 
 include_once __DIR__.'/utils/clsParams.php';
+include_once __DIR__.'/utils/clsProperties.php';
 
+$obj_simpleXML = new Properties();
+
+$obj_simpleXML -> readXML("../xml/dbxml.xml");
 
 $obj_Params = new Params();
 
